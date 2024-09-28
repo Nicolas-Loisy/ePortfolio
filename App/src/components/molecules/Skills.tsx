2e27265e8b00e673917c6c01ca175df3c0ec48fb
Nicolas L <nicolas.loisy@yahoo.fr>
2024-09-29 01:10:33 +0200

    Add Skills component

---
+++ App/src/components/molecules/Skills.tsx
@@ -0,0 +1,124 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  FaPython,
+  FaPhp,
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+} from "react-icons/fa"; // Ajouter d'autres icônes si nécessaire
+import {
+  SiSymfony,
+  SiMysql,
+  SiMongodb,
+  SiNeo4J,
+  SiGit,
+  SiGnubash,
+  SiDocker,
+  SiHuggingface,
+} from "react-icons/si"; // Icônes supplémentaires
+
+// Conteneur principal pour les compétences
+const SkillsContainer = styled.div`
+  display: flex;
+  flex-direction: column; // Change pour afficher le titre au-dessus
+  align-items: flex-start; // Aligne le titre à gauche
+  padding: 2rem; // Espacement autour du conteneur
+  box-sizing: border-box;
+  width: fit-content; // La largeur s'adapte au contenu
+  margin: 0 auto; // Centre le conteneur horizontalement
+`;
+
+// Conteneur pour les cartes avec largeur maximale
+const CardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap; // Permet aux cartes de passer à la ligne si nécessaire
+  justify-content: center; // Centre les cartes horizontalement
+  max-width: 1200px; // Largeur maximale des cartes
+  width: 100%; // Prend toute la largeur disponible
+`;
+
+// Style pour chaque carte de compétence
+const SkillCard = styled.div`
+  background-color: ${(props) =>
+    props.theme.cardBackground}; // Couleur de fond de la carte
+  border: 1px solid ${(props) => props.theme.borderColor}; // Bordure de la carte
+  border-radius: 8px; // Coins arrondis
+  padding: 1rem; // Espacement interne
+  margin: 1rem; // Marge entre les cartes
+  flex: 1 1 150px; // Permet aux cartes de s'étendre et de se réduire
+  max-width: 150px; // Largeur maximale de chaque carte
+  text-align: center; // Centre le texte
+  transition: transform 0.3s; // Animation au survol
+
+  &:hover {
+    transform: scale(1.05); // Grossissement au survol
+  }
+
+  @media (max-width: 768px) {
+    flex: 1 1 50px; // Permet aux cartes de s'étendre et de se réduire
+
+    max-width: 150px; // Largeur maximale de chaque carte
+    padding: 0.5rem; // Espacement interne
+  }
+`;
+
+// Style pour le titre de la compétence
+const SkillTitle = styled.h3`
+  font-size: 1rem; // Taille de la police
+  color: ${(props) => props.theme.text}; // Couleur du texte
+  margin: 0; // Pas de marge
+`;
+
+// Style pour l'icône de compétence
+const SkillIcon = styled.div`
+  font-size: 2rem; // Taille de l'icône
+  margin-bottom: 0.5rem; // Espace entre l'icône et le titre
+  color: ${(props) => props.theme.text}; // Couleur de l'icône
+`;
+
+// Style pour le titre
+const SectionTitle = styled.h2`
+  font-size: 2.5rem; // Taille du titre
+  color: ${(props) => props.theme.text}; // Couleur du texte
+  margin-bottom: 1rem; // Espacement en bas du titre
+  border-bottom: 5px solid #f9d342; // Ligne sous le titre
+  padding-bottom: 0.5rem; // Espacement intérieur
+  align-self: flex-start; // Aligne le titre à gauche
+  width: fit-content; // La largeur du titre s'adapte à son contenu
+`;
+
+const skills = [
+  { name: "Python", icon: <FaPython /> },
+  { name: "PHP", icon: <FaPhp /> },
+  { name: "Symfony", icon: <SiSymfony /> },
+  { name: "HTML", icon: <FaHtml5 /> },
+  { name: "CSS", icon: <FaCss3Alt /> },
+  { name: "JavaScript", icon: <FaJsSquare /> },
+  { name: "IA", icon: <SiHuggingface /> },
+  { name: "MySQL", icon: <SiMysql /> },
+  { name: "MongoDB", icon: <SiMongodb /> },
+  { name: "Neo4J", icon: <SiNeo4J /> },
+  { name: "Git", icon: <SiGit /> },
+  { name: "Bash", icon: <SiGnubash /> },
+  { name: "Docker", icon: <SiDocker /> },
+];
+
+const Skills: React.FC<{ title: string }> = ({ title }) => {
+  return (
+    <SkillsContainer>
+      <SectionTitle>{title}</SectionTitle>
+      {/* Conteneur pour les cartes */}
+      <CardsContainer>
+        {skills.map((skill) => (
+          <SkillCard key={skill.name}>
+            <SkillIcon>{skill.icon}</SkillIcon> {/* Affiche l'icône */}
+            <SkillTitle>{skill.name}</SkillTitle>
+          </SkillCard>
+        ))}
+      </CardsContainer>
+    </SkillsContainer>
+  );
+};
+
+export default Skills;
--- App/src/pages/HomePage.tsx
+++ App/src/pages/HomePage.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import Layout from "../components/templates/Layout";
-import Banner from "../components/molecules/Banner";
-import MainImage from "../components/atoms/MainImage";
 import InfoSection from "../components/organisms/InfoSection";
 import { useTranslation } from "react-i18next";
-import bannerImageDark from "../assets/img/LOGO_NL_dark.svg";
-import bannerImageLight from "../assets/img/LOGO_NL_white.svg";
-import NavLink from "../components/atoms/NavLink";
-import { FaGithub } from "react-icons/fa";
 import ProfileCard from "../components/molecules/ProfileCard";
 import pp from "../assets/img/pp_nl.jpg";
 import SocialBar from "../components/molecules/SocialBar";
 import About from "../components/molecules/About";
+import Skills from "../components/molecules/Skills";
 
 const HomePage: React.FC = () => {
   const { t } = useTranslation();
@@ -44,15 +39,7 @@ const HomePage: React.FC = () => {
         description="Je suis développeur web passionné par l'informatique dans son ensemble. Ma curiosité me pousse à explorer divers domaines tels que la technologie, l'intelligence artificielle et l'électronique. J'aime trouver des solutions à des problèmes techniques et relever des défis.<br /><br /> Chaque jour, j'ai la chance de travailler dans un environnement stimulant où je peux appliquer mes compétences tout en apprenant constamment. Mes passions pour l'informatique et la technologie me permettent de prendre du plaisir à créer et à innover, tout en m'enrichissant en connaissances et en expériences. Mon objectif est de continuer à évoluer dans ce domaine fascinant."
       />
 
-      <InfoSection
-        leftTitle="Documentation"
-        leftText="Accédez facilement à toute la documentation dont vous avez besoin. Des guides pratiques et des explications claires pour tout maîtriser."
-        rightTitle="IA & Machine Learning RAG"
-        rightText="Notre IA, basée sur le modèle de génération augmentée de récupération (RAG), analyse les documentations pour répondre instantanément à vos questions. Posez votre question, elle trouvera la réponse dans les moindres détails."
-      />
-      {/* <NavLink to="https://github.com/Nicolas-Loisy">
-        <FaGithub size={24} />
-      </NavLink> */}
+      <Skills title="Compétences"></Skills>
     </Layout>
   );
 };
