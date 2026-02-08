2caeefd01e944d557af4127ebed3909b13ea9e1c
Nicolas L <nicolas.loisy@yahoo.fr>
2026-02-08 01:25:38 +0100

    Add ScrollToTop component to reset scroll position on route change
    - Created a new ScrollToTop component that uses useEffect to scroll to the top of the page whenever the pathname changes.
    - Integrated ScrollToTop into the main Router component to ensure it functions across all routes.

--- App/src/router/Router.tsx
+++ App/src/router/Router.tsx
@@ -6,10 +6,12 @@ import Error404 from "../pages/error/Error404";
 import ErrorGeneric from "../pages/error/ErrorGeneric";
 import EnigmaPage from "../pages/EnigmaPage";
 import ProjectPage from "../pages/ProjectPage";
+import ScrollToTop from "../components/atoms/ScrollToTop";
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/403" element={<Error403 />} />
